5a5d85af834786d574b90030dffc636fbe340457
igm <yoshi.itsame@gmail.com>
2020-11-04 10:57:05 +0100

    cms results updated with new insertion policy

--- Results/convert.py
+++ Results/convert.py
@@ -13,16 +13,17 @@ import functools
 import pandas as pd
 
 smsdf = pd.read_table("asms_pipeline.out.tsv", sep='\t', header=None)
-#checkdf = pd.read_table("check_pipeline.out.tsv", sep='\t', header=None)
+checkdf = pd.read_table("check_pipeline.out.tsv", sep='\t', header=None)
 infodf = pd.read_table("info_pipeline.out.tsv", sep='\t', header=None)
 kmcdf = pd.read_table("kmc_pipeline.out.tsv", sep='\t', header=None)
-cmsdf = pd.read_table("cms_pipeline.out.tsv", sep='\t', header=None)
+#cmsdf = pd.read_table("cms_pipeline.out.tsv", sep='\t', header=None)
+cmsdf = pd.read_table("cmsg.out.tsv", sep='\t', header=None)
 bbhdf = pd.read_table("bbhash_pipeline.out.tsv", sep='\t', header=None)
 
 sizes = pd.read_csv("datasets_info.csv")
 
-smsdf.columns = ["name", "epsilon", "k", "ntrue", "threshold", "ssod", "savg", "smax", "ssize", "scsize"]
-#checkdf.columns = ["name", "epsilon", "k", "ncolls", "ntrue", "sod", "avg", "max"]
+smsdf.columns = ["name", "epsilon", "k", "skew", "threshold", "ssod", "savg", "smax", "ssize", "scsize"]
+checkdf.columns = ["name", "epsilon", "k", "ncolls", "ntrue", "sod", "avg", "max"]
 infodf.columns = ["name", "epsilon", "k", "R", "B", "RB"]
 kmcdf.columns = ["name", "k", "kmc"]
 cmsdf.columns = ["name", "epsilon", "k", "cntrue", "threshold", "csod", "cavg", "cmax", "csize", "ccsize"]
@@ -33,11 +34,11 @@ smsdf = smsdf.drop("skew", 1)
 smsdf["name"] = smsdf["name"].map(lambda x: "USakai" if x == "U_Sakai" else x)
 smsdf["name"] = smsdf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" else x)
 
-#checkdf = checkdf.drop("sod", 1)
-#checkdf = checkdf.drop("avg", 1)
-#checkdf = checkdf.drop("max", 1)
-#checkdf["name"] = checkdf["name"].map(lambda x: "USakai" if x == "U_Sakai" else x)
-#checkdf["name"] = checkdf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" else x)
+checkdf = checkdf.drop("sod", 1)
+checkdf = checkdf.drop("avg", 1)
+checkdf = checkdf.drop("max", 1)
+checkdf["name"] = checkdf["name"].map(lambda x: "USakai" if x == "U_Sakai" else x)
+checkdf["name"] = checkdf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" else x)
 
 infodf["name"] = infodf["name"].map(lambda x: "USakai" if x == "U_Sakai" else x)
 infodf["name"] = infodf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" else x)
@@ -45,7 +46,7 @@ infodf["name"] = infodf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" 
 kmcdf["name"] = kmcdf["name"].map(lambda x: "USakai" if x == "U_Sakai" else x)
 kmcdf["name"] = kmcdf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" else x)
 
-cmsdf = cmsdf.drop("skew", 1)
+#cmsdf = cmsdf.drop("skew", 1)
 cmsdf["name"] = cmsdf["name"].map(lambda x: "USakai" if x == "U_Sakai" else x)
 cmsdf["name"] = cmsdf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" else x)
 
@@ -56,18 +57,20 @@ bbhdf["name"] = bbhdf["name"].map(lambda x: "SRR622461" if x == "SRR622461_1" el
 
 #joined = functools.reduce(lambda  left,right: pd.merge(left, right, on=["name", "epsilon", "k"], how="outer"), ldfs)
 joined = pd.merge(smsdf, infodf, on=["name", "epsilon", "k"], how="outer")
+joined = pd.merge(joined, checkdf, on=["name", "epsilon", "k"], how="outer")
 joined = pd.merge(joined, cmsdf, on=["name", "epsilon", "k", "threshold"], how="outer")
 joined = pd.merge(joined, kmcdf, on=["name", "k"], how="outer")
 joined = pd.merge(joined, bbhdf, on=["name", "k"], how="outer")
 joined = pd.merge(joined, sizes, on=["name", "k"], how="outer")
 joined["tavg"] = joined["ssod"] / joined["ntrue"]
 #joined["cntrue"] = (round(joined["csod"] / joined["cavg"])).astype(int)
-joined["ntrue"] = round(joined["ntrue"] / joined["L0"] * 100)
+joined["ntrue"] = round(joined["ntrue"] / joined["L0"] * 100, 1)
 #joined["cntrue"] = round(joined["csod"] / (joined["cavg"] * joined["L0"]) * 100)
 joined["cntrue"] = round(joined["cntrue"] / joined["L0"] * 100)
 joined["savg"] = joined["savg"].map(lambda x: '{:.2f}'.format(x))
 joined["cavg"] = joined["cavg"].map(lambda x: '{:.2f}'.format(x))
 joined["tavg"] = joined["tavg"].map(lambda x: '{:.2f}'.format(x))
-joined = joined[["name", "epsilon", "k", "R", "B", "threshold", "ntrue", "ssod", "savg", "tavg", "smax", "cntrue", "csod", "cavg", "cmax", "kmc", "ssize", "scsize", "csize", "ccsize", "mphfsize", "bsize", "bcsize"]]
+joined = joined[["name", "epsilon", "k", "R", "B", "threshold", "ntrue", "ssod", "tavg", "smax", "cntrue", "csod", "cavg", "cmax", "kmc", "ssize", "scsize", "csize", "ccsize", "mphfsize", "bsize", "bcsize"]]
+joined.to_csv("fress_all_supplementary.csv", header=True, index=False)
 joined = joined[joined.k != 8]
 joined.to_csv("fress_all.csv", header=True, index=False)
